acddd6c77707796248386c7fd6e06968d2b97a06
MaxScarlet <maxim.kulik99@gmail.com>
2024-02-04 02:23:02 +0200

    File upload changes

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -16,9 +16,14 @@ import { Observable } from 'rxjs';
 export class FileUploadComponent {
   selectedFile: File | null = null;
   uploadProgress: number | undefined;
-  baseUrl = 'https://9q4yvdedcj.execute-api.il-central-1.amazonaws.com/stg';
+  private baseUrl = 'https://9q4yvdedcj.execute-api.il-central-1.amazonaws.com/stg';
+  // baseUrl = 'http://localhost:4063';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
+
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+  });
 
   onFileSelected(event: any): void {
     this.selectedFile = event.target.files[0];
@@ -34,12 +39,9 @@ export class FileUploadComponent {
 
     const fileName = this.selectedFile.name;
 
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json', // Set the content type based on your API's requirements
-    });
 
     const options = {
-      headers: headers,
+      headers: this.headers,
       params: {
         fileName: fileName,
       },
@@ -53,78 +55,78 @@ export class FileUploadComponent {
       (response) => {
         console.log('Response(init): ', response);
         const uploadId = response.uploadId;
-        this.uploadParts(uploadId);
+        if (!this.selectedFile) {
+          console.error('No file selected');
+          return;
+        }
+
+        const partSize = 5 * 1024 * 1024; // 5 MB part size (adjust as needed)
+        const totalParts = Math.ceil(this.selectedFile.size / partSize);
+        let uploadedPartsCnt = 0;
+        const uploadedParts: any[] = [];
+
+        for (let partNumber = 1; partNumber <= totalParts; partNumber++) {
+          const start = (partNumber - 1) * partSize;
+          const end = Math.min(partNumber * partSize, this.selectedFile.size);
+
+          const filePart = this.selectedFile.slice(start, end);
+          const formData = new FormData();
+          formData.append('file', filePart);
+
+          const headers = new HttpHeaders();
+          headers.set('Content-Type', 'multipart/form-data');
+
+          const options = {
+            headers: headers,
+            params: {
+              fileName: fileName,
+              uploadId: uploadId,
+              partNumber: partNumber.toString(),
+            },
+            reportProgress: true,
+          };
+
+          const uploadUrl = `${this.baseUrl}/upload`; // Replace with your API endpoint
+
+          this.http.post<any>(uploadUrl, formData, options).subscribe(
+            (eTagObj: any) => {
+              console.log('Response(uploadParts): ', eTagObj);
+              // TODO: reimplement
+              // this.uploadProgress = Math.round( (eTagObj.loaded / eTagObj.total!) * 100  );
+
+              uploadedPartsCnt++;
+              uploadedParts.push(eTagObj.resp);
+
+              console.log("uploadedPartsCnt", uploadedPartsCnt);
+              console.log("uploadedParts", uploadedParts);
+
+              if (uploadedPartsCnt === totalParts) {
+                this.uploadComplete(fileName, uploadId, uploadedParts);
+              }
+            },
+            (error) => {
+              console.error('Error uploading file part', error);
+            }
+          );
+        }
       },
       (error: HttpErrorResponse) => {
         console.error('Error initiating multipart upload', error);
-        // Handle error, show error message to the user, etc.
       }
     );
   }
 
-  uploadParts(uploadId: string): void {
-    if (!this.selectedFile) {
-      console.error('No file selected');
-      return;
-    }
-
-    const partSize = 5 * 1024 * 1024; // 5 MB part size (adjust as needed)
-    const totalParts = Math.ceil(this.selectedFile.size / partSize);
-
-    let uploadedParts = 0;
-
-    for (let partNumber = 1; partNumber <= totalParts; partNumber++) {
-      const start = (partNumber - 1) * partSize;
-      const end = Math.min(partNumber * partSize, this.selectedFile.size);
-
-      const filePart = this.selectedFile.slice(start, end);
-      const formData = new FormData();
-      formData.append('file', filePart);
-      console.log('filePart:', filePart);
-
-      localStorage.setItem('formData', JSON.stringify({ data: filePart }));
-
-      const headers = new HttpHeaders();
-      headers.set('Content-Type', 'multipart/form-data');
-
-      const options = {
-        headers: headers,
-        params: {
-          uploadId: uploadId,
-          partNumber: partNumber.toString(),
-        },
-        reportProgress: true,
-      };
-
-      const uploadUrl = `${this.baseUrl}/upload`; // Replace with your API endpoint
-
-      this.http.post<any>(uploadUrl, formData, options).subscribe(
-        (event: HttpEvent<any>) => {
-          console.log('Response(uploadParts): ', event);
-
-          if (event.type === HttpEventType.UploadProgress) {
-            this.uploadProgress = Math.round(
-              (event.loaded / event.total!) * 100
-            );
-          } else if (event.type === HttpEventType.Response) {
-            uploadedParts++;
-
-            if (uploadedParts === totalParts) {
-              this.uploadComplete(uploadId);
-            }
-          }
-        },
-        (error) => {
-          console.error('Error uploading file part', error);
-        }
-      );
-    }
-  }
-
-  uploadComplete(uploadId: string): void {
+  uploadComplete(fileName: string, uploadId: string, parts: any[]): void {
     const completeUrl = `${this.baseUrl}/complete`; // Replace with your API endpoint
-
-    this.http.post<any>(completeUrl, { uploadId }).subscribe(
+    const options = {
+      headers: this.headers,
+      params: {
+        fileName: fileName,
+        uploadId: uploadId,
+      },
+      reportProgress: true,
+    };
+    this.http.post<any>(completeUrl, {parts}, options).subscribe(
       (response) => {
         console.log('Response(uploadComplete): ', response);
         console.log('Multipart upload completed successfully', response);
