ed2a1034bf7fd770b2dc241381085287c554d56d
MaxScarlet <maxim.kulik99@gmail.com>
2024-02-16 13:33:39 +0200

    Auth0 settings

--- src/environment.stg.ts
+++ src/environment.stg.ts
@@ -2,4 +2,9 @@ export const environment = {
   podcastUrl: 'https://4oy063kj1f.execute-api.il-central-1.amazonaws.com/dev',
   episodeUrl: 'https://46iwk2wgsc.execute-api.il-central-1.amazonaws.com/dev',
   fileMngUrl: 'https://9q4yvdedcj.execute-api.il-central-1.amazonaws.com/dev',
+  
+  auth0: {
+    domain: 'oxymoron-tech.eu.auth0.com',
+    id: '--stg--' 
+  }
 };
--- src/environment.ts
+++ src/environment.ts
@@ -2,4 +2,10 @@ export const environment = {
   podcastUrl: 'http://localhost:4061',
   episodeUrl: 'http://localhost:4062',
   fileMngUrl: 'http://localhost:4063',
+  feedMngUrl: 'http://localhost:4064',
+
+  auth0: {
+    domain: 'oxymoron-tech.eu.auth0.com',
+    id: '9lbgWvJYmVXtSpUa2LOqybe9mZkfagl6' 
+  }
 };
